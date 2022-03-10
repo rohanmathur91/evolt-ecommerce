794468d1099b1a3c4d18cf2d054ab38861224847
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-10 11:21:20 +0530

    feat: add product type filters

--- src/context/utilities/getFilteredData.js
+++ src/context/utilities/getFilteredData.js
@@ -5,5 +5,14 @@ export const getFilteredData = (
 	return products
 		.filter(({ price }) => Number(price) <= filterPrice)
 		.filter(({ inStock }) => (inStockOnly ? inStock : true))
-		.filter(({ brand }) => (filterBrands[brand] ? brand : true));
+		.filter(({ brand }) => {
+			return Object.keys(filterBrands).some((brand) => filterBrands[brand])
+				? filterBrands[brand]
+				: true;
+		})
+		.filter(({ type }) => {
+			return Object.keys(types).some((type) => types[type])
+				? types[type]
+				: true;
+		});
 };
