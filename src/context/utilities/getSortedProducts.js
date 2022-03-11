0cb86d6d42ccaa3c4272092a954f798f0b4ae89d
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-11 12:15:48 +0530

    refactor: sorting product function

--- src/context/utilities/getSortedProducts.js
+++ src/context/utilities/getSortedProducts.js
@@ -1,12 +1,17 @@
 export const getSortedProducts = (products, { sortBy }) => {
-	if (sortBy === "SORT_BY_LATEST") {
-		return [...products]
-			.sort((a, b) => (a.latest === b.latest ? 0 : a ? -1 : 1))
-			.reverse();
-	} else if (sortBy === "SORT_BY_LOW_TO_HIGH") {
-		return [...products].sort((a, b) => a.price - b.price);
-	} else if (sortBy === "SORT_BY_HIGH_TO_LOW") {
-		return [...products].sort((a, b) => b.price - a.price);
+	switch (sortBy) {
+		case "SORT_BY_LATEST":
+			return [...products]
+				.sort((a, b) => (a.latest === b.latest ? 0 : a ? -1 : 1))
+				.reverse();
+
+		case "SORT_BY_LOW_TO_HIGH":
+			return [...products].sort((a, b) => a.price - b.price);
+
+		case "SORT_BY_HIGH_TO_LOW":
+			return [...products].sort((a, b) => b.price - a.price);
+
+		default:
+			return products;
 	}
-	return products;
 };
