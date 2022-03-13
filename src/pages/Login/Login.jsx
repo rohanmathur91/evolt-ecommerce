302b91c8cdacf1419da388d136e3d9c8afab10a5
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-13 17:39:50 +0530

    feat: add login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { Input } from "../../components";
 import "./Login.css";
 
@@ -8,42 +9,39 @@ export const Login = () => {
 		password: "",
 	});
 
+	const handleChange = (event, field) => {
+		setCredentials((prevCredentials) => ({
+			...prevCredentials,
+			[field]: event.target.value,
+		}));
+	};
+
+	// TODO: For remember me, increase the expriy of JWT token from backend
+
 	return (
 		<div className="w-100 flex-row content-center mt-4">
-			<form
-				className="login-form py-2 px-4 w-100 card-shadow rounded-sm m-2"
-				action="./login.html"
-			>
+			<form className="login-form py-2 px-4 w-100 card-shadow rounded-sm m-2">
 				<h3 className="text-center mt-2 mb-3">Login</h3>
-				<div className="flex-column mb-3">
-					<label for="email">Email address</label>
-					<input
-						className="mt-1 py-1 px-2 text-base border rounded-sm"
-						id="email"
-						type="email"
-						placeholder="Enter your email"
-					/>
-				</div>
+				<Input
+					id="email"
+					type="email"
+					title="Email address"
+					placeholder="Enter your email"
+					updateValue={handleChange}
+				/>
 
-				<div className="flex-column mb-3">
-					<label for="password">Password</label>
-					<input
-						className="mt-1 py-1 px-2 text-base border rounded-sm"
-						id="password"
-						type="password"
-						placeholder="Enter your password"
-					/>
-				</div>
-
+				<Input
+					id="password"
+					type="password"
+					title="Password"
+					placeholder="Enter your password"
+					updateValue={handleChange}
+				/>
 				<div className="login-remember-me flex-row content-space-between my-2">
-					<label for="remember-me">
+					<label htmlFor="remember-me">
 						<input className="mr-1" type="checkbox" id="remember-me" /> Remember
 						me
 					</label>
-
-					<a className="forgot-password font-bold" href="#">
-						Forgot your Password?
-					</a>
 				</div>
 
 				<button className="p-1 w-100 font-semibold btn btn-solid transition-2 mr-1 mb-2 rounded-sm">
@@ -51,9 +49,9 @@ export const Login = () => {
 				</button>
 
 				<div className="text-center mb-2">
-					<a href="./signup.html">
+					<Link to="/signup">
 						Create New Account <i className="fa fa-chevron-right"></i>
-					</a>
+					</Link>
 				</div>
 			</form>
 		</div>
