16c01ff69b5191796f63fe0e67a17dad91d318c4
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-13 18:28:03 +0530

    refactor: page exports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-export { Home } from "./Home/Home";
-export { Products } from "./Products/Products";
-export { Wishlist } from "./Wishlist/Wishlist";
 export { Cart } from "./Cart/Cart";
+export { Home } from "./Home/Home";
 export { Login } from "./Login/Login";
 export { Signup } from "./Signup/Signup";
+export { Products } from "./Products/Products";
+export { Wishlist } from "./Wishlist/Wishlist";
