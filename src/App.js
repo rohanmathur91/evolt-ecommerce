7e49661e85324897f445cf36b6cab3164b572b7f
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-15 13:30:39 +0530

    chore: initial project setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import "./App.css";
-import { Home } from "./pages";
 
-function App() {
-	return (
-		<div className="App">
-			<Home />
-		</div>
-	);
-}
+const App = () => {
+	return <div className="App">App</div>;
+};
 
 export default App;
