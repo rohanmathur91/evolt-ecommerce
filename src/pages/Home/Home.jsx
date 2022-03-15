5b078b3d29cbb040722b669e93d2d79b849f4833
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-15 13:46:34 +0530

    feat: add home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useDocumentTitle } from "../../hooks";
+import { Category, BrandList, Footer } from "../../components";
+import banner from "../../assets/banner.jpg";
+import "./Home.css";
+
+export const Home = () => {
+	useDocumentTitle("Evolt | Home");
+
+	return (
+		<div>
+			<header className="overflow-hidden gutter-bottom">
+				<img className="banner w-100" src={banner} alt="banner" />
+			</header>
+
+			<section className="category-container m-auto p-1 gutter-bottom ">
+				<Category />
+				<BrandList />
+			</section>
+			<Footer />
+		</div>
+	);
+};
