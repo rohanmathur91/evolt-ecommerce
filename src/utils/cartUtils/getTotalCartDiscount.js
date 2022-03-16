24a7a8e50c4a1acabf767e4b18119ba1ac36bbe5
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-16 21:39:52 +0530

    feat: add cart context utils

---
+++ src/utils/cartUtils/getTotalCartDiscount.js
@@ -0,0 +1,15 @@
+/**
+ * Calculates total discount of whole cart if cart exist else 500 will be
+ * default discount.
+ * @param  {array} cart
+ * @param  {number} totalPrice
+ * @returns {number} discount
+ */
+export const getTotalCartDiscount = (cart, totalPrice) => {
+	return cart.length > 0
+		? (cart.reduce((total, { discount }) => total + discount, 0) /
+				cart.length /
+				100) *
+				totalPrice
+		: 500;
+};
