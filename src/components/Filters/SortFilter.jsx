fe9e1e3a7d6b26885e762ab5d109e6c43299135a
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-16 17:41:37 +0530

    feat: add filters for product

---
+++ src/components/Filters/SortFilter.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useProduct } from "../../context";
+import { SortFilterList } from "./SortFilterList";
+
+export const SortFilter = () => {
+	const { productDispatch } = useProduct();
+
+	return (
+		<>
+			<div className="flex-row content-space-between pt-2">
+				<div className="filter-title font-bold">Filters</div>
+				<button
+					onClick={() => productDispatch({ type: "CLEAR_FILTER" })}
+					className="filter-clear-btn text-base font-bold"
+				>
+					Clear all
+				</button>
+			</div>
+			<div className="filter-header py-2 mt-2 text-base font-bold">Sort By</div>
+			<SortFilterList />
+		</>
+	);
+};
