2d418b73544e9c3ab543a590f471d2d849066dba
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-17 11:42:05 +0530

    feat: add login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Input } from "../../components";
+import "./Login.css";
+
+export const Login = () => {
+	const [credentials, setCredentials] = useState({
+		email: "",
+		password: "",
+	});
+
+	const handleInputChange = (event, field) => {
+		setCredentials((prevCredentials) => ({
+			...prevCredentials,
+			[field]: event.target.value,
+		}));
+	};
+
+	const handleFormSubmit = (event) => {
+		event.preventDefault();
+	};
+
+	return (
+		<div className="w-100 flex-row content-center mt-4">
+			<form
+				onSubmit={handleFormSubmit}
+				className="login-form py-2 px-4 w-100 card-shadow rounded-sm m-2"
+			>
+				<h3 className="text-center mt-2 mb-3">Login</h3>
+				<Input
+					id="email"
+					type="email"
+					title="Email address"
+					value={credentials.email}
+					placeholder="Enter your email"
+					updateValue={handleInputChange}
+				/>
+
+				<Input
+					id="password"
+					type="password"
+					title="Password"
+					value={credentials.password}
+					placeholder="Enter your password"
+					updateValue={handleInputChange}
+				/>
+
+				<button className="p-1 w-100 font-semibold btn btn-solid transition-2 mr-1 mb-2 rounded-sm">
+					Login
+				</button>
+
+				<div className="text-center mb-2">
+					<Link to="/signup">
+						Create New Account <i className="fa fa-chevron-right"></i>
+					</Link>
+				</div>
+			</form>
+		</div>
+	);
+};
