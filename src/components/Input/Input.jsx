c7ad15fc6edf507bc75e3b2c3dcc309ba3c9fa6d
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-18 12:21:03 +0530

    feat: add default props for input component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -18,18 +18,29 @@ export const Input = ({
 				required
 				id={id}
 				type={type}
-				placeholder={placeholder}
-				className="mt-1 py-1 px-2 text-base border rounded-sm"
 				value={value}
+				placeholder={placeholder}
 				onChange={(event) => updateValue(event, id)}
+				className="mt-1 py-1 px-2 text-base border rounded-sm"
 			/>
 
 			{error && (
-				<div className="validation-msg flex-row items-center mt-1 error">
+				<div className="error validation-msg flex-row items-center mt-1">
 					<span className="material-icons-outlined mr-1">error_outline</span>{" "}
 					{error}
 				</div>
 			)}
 		</div>
 	);
+};
+
+Input.defaultProps = {
+	id: "",
+	type: "",
+	title: "",
+	value: "",
+	placeholder: "",
+	updateValue: () => {},
+	error: "",
+	showPassword: "",
 };
