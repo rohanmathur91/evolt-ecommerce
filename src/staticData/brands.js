aa60988f987260a1eccf27d393a6c2ee5dd3e74a
rohanmathur91 <mathurrohan04@gmail.com>
2022-03-31 16:31:18 +0530

    fix: banner import

--- src/staticData/brands.js
+++ src/staticData/brands.js
@@ -11,10 +11,12 @@ export const brands = [
       "https://raw.githubusercontent.com/rohanmathur91/evolt-ecommerce/dev/public/images/sony.png",
   },
   {
-    image: "https://upload.wikimedia.org/wikipedia/commons/b/bc/JBL_logo.svg",
+    image:
+      "https://raw.githubusercontent.com/rohanmathur91/evolt-ecommerce/b2ac08159cd8726497d32de034f9c5737e12b053/public/images/jbl.svg",
   },
 
   {
-    image: "https://support.boat-lifestyle.com/images/Boat-Logo_2.png",
+    image:
+      "https://raw.githubusercontent.com/rohanmathur91/evolt-ecommerce/dev/public/images/boat.png",
   },
 ];
