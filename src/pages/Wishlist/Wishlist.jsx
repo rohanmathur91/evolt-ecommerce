d315dc2a5109b2b2d43a93308d5488d2ca64b463
rohanmathur91 <mathurrohan04@gmail.com>
2022-04-01 02:02:12 +0530

    fix: loader for wishlist

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,19 +1,45 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { useCart } from "../../contexts";
+import { INITIALIZE_WISHLIST } from "../../reducer";
 import { useScrollToTop, useDocumentTitle } from "../../hooks";
 import { ProductCard } from "../../components";
 import "./Wishlist.css";
 
 export const Wishlist = () => {
-  const { wishlist } = useCart();
+  const [loader, setLoader] = useState(false);
+  const { wishlist, cartDispatch } = useCart();
 
   useScrollToTop();
   useDocumentTitle("Wishlist");
 
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoader(true);
+        const {
+          data: { wishlist },
+        } = await axios.get("/api/user/wishlist", {
+          headers: { authorization: localStorage.getItem("token") },
+        });
+
+        cartDispatch({
+          type: INITIALIZE_WISHLIST,
+          payload: wishlist,
+        });
+        setLoader(false);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
   return (
     <div>
       <h3 className="mt-6 mb-3 text-center">My Wishlist</h3>
-      {wishlist.length ? (
+      {loader ? (
+        <h4 className="text-center mt-2 p-1">Loading wishlist...</h4>
+      ) : wishlist.length ? (
         <div className="products w-100 p-1 mt-2 mb-6">
           {wishlist.map((product) => (
             <ProductCard key={product._id} product={product} />
