ced27d48156b9ddee20a503d2cb3f9f420cc1335
rohanmathur91 <mathurrohan04@gmail.com>
2022-04-02 19:58:33 +0530

    fix: exports for static data

---
+++ src/staticData/addressFormData.js
@@ -0,0 +1,11 @@
+export const address = {
+  country: "India",
+  pinCode: "400030",
+  fullName: "Rohan Mathur",
+  cityAndState: "Bangalore",
+  mobileNumber: "9922334455",
+  home: "Room no.1, grd flr",
+  area: "Sai Sundar, Century Bazaar",
+};
+
+export const countries = ["Hungary", "Iceland", "India", "Indonesia", "Japan"];
--- src/staticData/countries.js
+++
@@ -1,1 +0,0 @@
-export const countries = ["Hungary", "Iceland", "India", "Indonesia", "Japan"];
