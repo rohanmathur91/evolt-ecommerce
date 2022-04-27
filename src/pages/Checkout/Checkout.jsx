72a3821fd83633b0efba488c6756310d405fdf65
rohanmathur91 <mathurrohan04@gmail.com>
2022-04-27 15:05:21 +0530

    fix: razorpay modal issue

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -35,9 +35,9 @@ export const Checkout = () => {
     return new Promise((resolve) => {
       const script = document.createElement("script");
       script.src = src;
+      document.body.appendChild(script);
       script.onload = () => resolve(true);
       script.onerror = () => resolve(false);
-      document.body.appendChild(script);
     });
   };
 
@@ -93,6 +93,9 @@ export const Checkout = () => {
 
     const paymentObject = new window.Razorpay(options);
     paymentObject.open();
+    paymentObject.on("payment.failed", () => {
+      showToast("error", "Payment failed, please try again.");
+    });
   };
 
   return !cartProducts.length ? (
